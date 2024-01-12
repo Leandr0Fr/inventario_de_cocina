299811493d93fc1fb47e620908f9230fa369a198
LeandroFr <leandro00fr@gmail.com>
2024-01-12 16:52:24 -0300

    CREATED heladera model

--- apps/heladera/admin.py
+++ apps/heladera/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Heladera
 
-# Register your models here.
+class HeladeraAdmin(admin.ModelAdmin):
+    list_display = ("name", "input_date", "expiration_date", "amount")
+    search_fields = ("name",)
+    list_filter = ("input_date", "expiration_date")
+
+admin.site.register(Heladera, HeladeraAdmin)
+    
--- apps/heladera/models.py
+++ apps/heladera/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+class Heladera(models.Model):
 
-# Create your models here.
+    name = models.CharField(max_length=20)
+    input_date = models.DateField(auto_now_add=True)
+    expiration_date = models.DateField()
+    weight = models.FloatField(null=True, blank=True)
+    liters = models.FloatField(null=True, blank=True)
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return f"name: {self.name} - input_date: {self.input_date} - expiration_date: {self.expiration_date} - weight: {self.weight} - liters: {self.liters} - amount: {self.amount}"
