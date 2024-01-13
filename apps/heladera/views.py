5ae7790d801bcaf225e609c5636ad819258bce66
LeandroFr <leandro00fr@gmail.com>
2024-01-13 09:00:46 -0300

    ADDED urls Heladera

--- apps/heladera/views.py
+++ apps/heladera/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Heladera
+from utils.pagination_heladera import Pag_heladera
 
-# Create your views here.
+def view_page(request):
+    pag = Pag_heladera()
+    items = pag.get_items()
+    return render(request, 'heladera.html', {'list_items': items})
+
+def create_item(request):
+    if request.method == 'GET':
+        raise ValueError("Get method is not accepted")
