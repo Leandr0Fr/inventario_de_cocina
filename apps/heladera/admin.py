2e4e82b42b06175baaab5ee9e14b97c48e15bd53
LeandroFr <leandro00fr@gmail.com>
2024-01-13 16:31:24 -0300

    UPDATED heladera model

--- apps/heladera/admin.py
+++ apps/heladera/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Heladera
 
 class HeladeraAdmin(admin.ModelAdmin):
-    list_display = ("name", "input_date", "expiration_date", "amount")
-    search_fields = ("name",)
-    list_filter = ("input_date", "expiration_date")
+    list_display = ("name", "input_date", "expiration_date", "amount", "status")
+    search_fields = ("name", "status")
+    list_filter = ("input_date", "expiration_date", "status")
 
 admin.site.register(Heladera, HeladeraAdmin)
     
--- apps/heladera/models.py
+++ apps/heladera/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 class Heladera(models.Model):
 
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=15)
     input_date = models.DateField(auto_now_add=True)
     expiration_date = models.DateField(null=True, blank=True)
     weight = models.FloatField(null=True, blank=True)
     liters = models.FloatField(null=True, blank=True)
     amount = models.IntegerField()
+    status = models.CharField(max_length=15, default="Buen Estado")
 
     def __str__(self):
-        return f"name: {self.name} - input_date: {self.input_date} - expiration_date: {self.expiration_date} - weight: {self.weight} - liters: {self.liters} - amount: {self.amount}"
+        return f"name: {self.name} ; input_date: {self.input_date} ; expiration_date: {self.expiration_date} ; weight: {self.weight} ; liters: {self.liters} ; amount: {self.amount} ; status: {self.status}"
