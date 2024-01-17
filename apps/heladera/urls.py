cbb212a130488bfc85afccea8556640b47e6c284
LeandroFr <leandro00fr@gmail.com>
2024-01-17 09:30:49 -0300

    ADDED url exists_name

--- apps/heladera/urls.py
+++ apps/heladera/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import view_page
+from .views import view_page, exists_name
 
 urlpatterns = [
     path('', view_page, name="heladera"),
     path('page/<int:number_page>/', view_page, name="heladera_pag"),
+    path('name/<str:name>/', exists_name, name="exists_name"),
+
 ]
