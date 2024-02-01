7fa1aedcf8086a317e71bcb2c4f1df723574e9a0
LeandroFr <leandro00fr@gmail.com>
2024-02-01 02:25:29 -0300

    REFACTOR open dialog

--- static/js/dialog.js
+++ static/js/dialog.js
@@ -1,39 +1,32 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const addButton = document.querySelector(".add-item-button");
-    const exitButton = document.querySelector(".exit-button");
-    const modal = document.querySelector("dialog");
+const addButton = document.querySelector(".add-item-button");
+const exitButton = document.querySelector(".exit-button");
+const modal = document.querySelector("dialog");
 
-    addButton.addEventListener("click", function() {
-        modal.style.display = "flex";
-        modal.showModal();
+addButton.addEventListener("click", () => {
+    modal.style.display = "flex";
+    modal.showModal();
     });
 
-    exitButton.addEventListener("click", function() {
-        closeModal();
+exitButton.addEventListener("click", () => {
+    closeModal();
     });
 
-    modal.addEventListener("click", function(event) {
-        const modalRect = modal.getBoundingClientRect();
-        const isInsideModal = (
-            event.clientX >= modalRect.left &&
-            event.clientX <= modalRect.right &&
-            event.clientY >= modalRect.top &&
-            event.clientY <= modalRect.bottom
-        );
+modal.addEventListener("click", (e) => {
+    const modalRect = modal.getBoundingClientRect();
+    const isInsideModal = (
+        e.clientX >= modalRect.left &&
+        e.clientX <= modalRect.right &&
+        e.clientY >= modalRect.top &&
+        e.clientY <= modalRect.bottom);
 
-        if (!isInsideModal) {
-            closeModal();
-        }
-    });
-
-    document.addEventListener("keydown", function(event) {
-        if (event.key === "Escape" && modal.style.display === "flex") {
-            closeModal();
-        }
-    });
-
-    function closeModal() {
-        modal.style.display = "none";
-        modal.close();
-    }
+    if (!isInsideModal) closeModal();    
 });
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape" && modal.style.display === "flex") closeModal();
+});
+
+const closeModal = () => {
+    modal.style.display = "none";
+    modal.close();
+}
