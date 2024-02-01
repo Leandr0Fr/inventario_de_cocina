212ab3d8cc6a6c23d28b0e072d6ed205a2df964a
LeandroFr <leandro00fr@gmail.com>
2024-02-01 02:25:14 -0300

    CHANGED object x map

--- static/js/viewItems.js
+++ static/js/viewItems.js
@@ -3,30 +3,30 @@ window.onload = () =>{
     const itemsContainer = document.getElementById("items-container");
     const itemList = itemsContainer.dataset.items.slice(0, -1).split(',');
 
-    let item = {};
-
-    itemList.forEach(item => {
-        let itemPar = item.trim().split(';');
-        itemPar.forEach(par => {
-            const [key, value] = par.split(':');
+    itemList.forEach(itemString => {
+        const itemMap = new Map();
+        const itemProperties = itemString.trim().split(';');
+        
+        itemProperties.forEach(property => {
+            const [key, value] = property.split(':');
             const keyItem = key ? key.trim(): null;
             const valueItem = value ? value.trim() : null;
             
-            item[keyItem] = valueItem; 
+            itemMap.set(keyItem, valueItem);
         });
 
         const card = document.createElement("div");
         card.className = "card";
-        unit = item.unit === "LT" ? "Litros": "Kilogramos";
+        const unit = itemMap.get('unit') === "LT" ? "Litros": "Kilogramos";
 
         card.innerHTML = `
-        <div class="head-card">${itemObject.name}</div>
+        <div class="head-card">${itemMap.get('name')}</div>
         <div class="divider"></div>
         <section class="data-item">
-            <p class="date">Ingreso: ${itemObject.input_date}</p>
-            <p class="date">Vencimiento: ${itemObject.expiration_date}</p>
-            <p class="amount">${unit}: ${itemObject.quantity}</p>
-            <p class="amount">Cantidad: ${itemObject.amount}</p>
+            <p class="date">Ingreso: ${itemMap.get('input_date')}</p>
+            <p class="date">Vencimiento: ${itemMap.get('expiration_date')}</p>
+            <p class="amount">${unit}: ${itemMap.get('quantity')}</p>
+            <p class="amount">Cantidad: ${itemMap.get('amount')}</p>
         </section>
         <section class="controller-card">
             <button class="button-card"><span>+</span> añadir</button>
@@ -35,7 +35,7 @@ window.onload = () =>{
                 <i class="gg-trash"></i></button>
         </section>
         <div class="status-container">
-            <p class="status">${itemObject.status}</p>
+            <p class="status">${itemMap.get('status')}</p>
         </div>`;
 
         cardsContainer.appendChild(card);
